7bd7f1d7d230ff4d5c185ecd32f3fc3832ee5066
DorMitzafon <dormitzafon@gmail.com>
2016-12-17 12:13:49 +0200

    worked on mission and screen
    also worked in TheSankesGame.cpp on run function

--- mission.h
+++ mission.h
@@ -9,9 +9,25 @@ using namespace std;
 
 class TheSnakesGame;
 
-class mission {
-	char* theMission;
+class Mission {
+	enum {PrimeNumber, DividedBy4, MultipleBy7, PowOf_Znum, RestIs3, Num169};
 	TheSnakesGame * theGame;
+	int numOfMiss;
 public:
+	Mission(int num);
+	Mission() {
+		this->numOfMiss = rand() % 5;
+	}
+	~Mission();
+	int getMissionNum() {
+		return numOfMiss;
+	}
+	BOOL isMissionOK(int missionNum, int num);
+	BOOL isM1(int num);
+	BOOL isM2(int num);
+	BOOL isM3(int num);
+	BOOL isM4(int num);
+	BOOL isM5(int num);
+	BOOL isM6(int num);
 };
 #endif
